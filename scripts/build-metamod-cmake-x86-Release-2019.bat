6d706b5fa9b23480debcfa1427f481c3045c8c34
Mikk155 <martimsanchez155@gmail.com>
2025-08-06 22:40:04 -0300

    batch file for VS16 2019

---
+++ scripts/build-metamod-cmake-x86-Release-2019.bat
@@ -0,0 +1,24 @@
+@echo off
+
+setlocal
+
+:: Check if SolutionDir is already set and non-empty
+if not defined SolutionDir (
+    :: Only set SolutionDir if it's not already set
+    SET "SolutionDir=%~dp0.."
+)
+
+:: Ensure the path ends with a backslash
+if not "%SolutionDir:~-1%"=="\" SET "SolutionDir=%SolutionDir%\"
+
+cd /d "%SolutionDir%"
+
+@REM cmake -G "Visual Studio 17 2022" -B "%SolutionDir%build-cmake\x86\Release" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%install\x86\Release" -DCMAKE_BUILD_TYPE=Release -S %SolutionDir%
+cmake -G "Visual Studio 16 2019" -B "%SolutionDir%build-cmake\x86\Release" -A Win32 -DCMAKE_INSTALL_PREFIX="%SolutionDir%install\x86\Release" -DCMAKE_BUILD_TYPE=Release -S %SolutionDir%
+
+cmake --build "%SolutionDir%build-cmake\x86\Release" --config Release --target install
+
+md "%SolutionDir%build\addons\metamod\dlls\"
+copy "%SolutionDir%install\x86\Release\bin\*.dll" "%SolutionDir%build\addons\metamod\dlls\"
+
+endlocal 
